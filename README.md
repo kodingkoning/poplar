9beba6edc2dfd801228a3546d3bafbbdbbf0c925
Elizabeth Koning <erkonin@sandia.gov>
2023-10-12 12:02:24 -0700

    complete draft

---
+++ README.md
@@ -0,0 +1,55 @@
+# Poplar
+
+## Purpose
+
+Popular is a software pipeline that uses an input of genes and assembled genomes and generates a phylogenetic tree from the input.
+
+## Dependencies
+
+Before running, you must have already installed:
+
+- [numpy](https://numpy.org/)
+- [sklearn](https://scikit-learn.org/stable/index.html)
+- [biopython](https://biopython.org/docs/1.75/api/Bio.html)
+
+Recommended installation:
+
+`pip install biopython numpy sklearn`
+
+## Input
+
+The pipeline accepts a JSON file matching the format of the `dataset_catalog.json` that comes with a download from NCBI's genome datasets. The JSON file specifies the relative locations of the file contianing FASTA files.
+
+### Downloading Data
+
+Visit [NCBI's Genome Datasets page](https://www.ncbi.nlm.nih.gov/datasets/genome) and search for the desired taxa. From there, check the boxes for the assemblies you are interested in. Click "Download" and "Download Package". Select "Genome sequences (FASTA)" and "Genomic coding sequences (FASTA)" and download your file. Unzip the file, and it will contain the relevant FASTA files and `dataset_catalog.json`.
+
+If you have additional data to include alongside NCBI genomes, add an entry in the file for each additional species. If you are not downloading data from NCBI, use `files/dataset_catalog.json` as a template for your input file. `filePath` should be relative to the location of `dataset_catalog.json`, and file types should be `CDS_NUCLEOTIDE_FASTA` for gene files and `GENOMIC_NUCLEOTIDE_FASTA` for assembled genomes.
+
+The "accession" field in the JSON file will be used for naming intermediate files as well as the placement in the output tree, so the values must be unique for each entry. If downloaded from NCBI, they will be GenBank IDs.
+
+### Running the Pipeline
+
+On a machine using Slurm, run `sbatch poplar.sbatch /path/to/dataset_catalog.json`.
+
+## Output
+
+The pipeline will create a new directory within the current directory to store all the temporary files. One output tree will be named `job_{jobnum}.tree` and be in the current directory. The labels of this tree will match the accession IDs. If `dataset_catalog.json` contains a file of type `DATA_TABLE`, then Aasecond output tree named `job_{jobnum}_scinames.tree` will replace the accession IDs with the scientific names. This may be easier to read, but it will not differentiate between multiple instances of a single species, and it will not rename any species without an entry in the data table.
+
+## Limitations
+
+This tool only works on Slurm machines.
+
+If certain pre-compiled executables (such as for seqkit) cannot run on the provided architecture, there will be errors. These can be resolved by replacing the executables in their current locations.
+
+Jobs spawn other jobs and then wait for their completition. If jobs are forced to wait in the queue beyond the time limit of the originating job, then the pipeline might crash.
+
+There are currently not any checkpointing implemented. If the pipeline crashes, then the preceeding steps need to be rerun rather than continue from where it was left off. (TODO: add checkpointing)
+
+TODO: make a way to pick the time limits and queues for all of the scripts
+
+TODO: take out the places where it activates the biopython env. List dependencies
+
+TODO: figure out why the full Kickxellomycotina set gets an error with one of the inputs (shows as "" query)
+
+TODO: add comments to code
---
+++ scripts/group.py
@@ -0,0 +1,92 @@
+import csv
+import numpy as np
+from sklearn.cluster import DBSCAN
+from scipy.sparse import lil_array
+from scipy.sparse import csr_array
+from sklearn.cluster import SpectralClustering
+from sklearn.metrics import silhouette_score
+from sklearn.neighbors import sort_graph_by_row_values
+from collections import defaultdict
+
+# Define the path to your BLAST results file
+blast_results_file = 'blast_results.csv'  # Replace with your file path
+
+# Define the minimum number of clusters as 1/N (N specified via command line)
+# Replace N with the appropriate value or use a command-line argument parser
+N = 100
+
+# Initialize dictionaries to map sequence IDs to matrix indices
+sequence_to_index = {}
+index_to_sequence = {}
+
+# Create a dictionary of dictionaries to store the similarity scores as a sparse matrix
+similarity_scores = {}
+
+# Read the BLAST results file and populate the dictionaries and similarity_scores
+with open(blast_results_file, 'r') as csvfile:
+    reader = csv.reader(csvfile, delimiter='\t')  # Adjust delimiter if needed
+    for row in reader:
+        query_sequence = row[0]  # Replace with the column index of the query sequence
+        subject_sequence = row[1]  # Replace with the column index of the subject sequence
+        similarity_score = float(row[2])  # Replace with the column index of the similarity score
+
+        # Split query_sequence and subject_sequence on ORF or gene
+        # Only store distance if the sequences are on two different species
+        if query_sequence.partition("_ORF")[0].partition("_gene")[0] != subject_sequence.partition("_ORF")[0].partition("_gene")[0]:
+
+            # Check if the query sequence is in the dictionary; if not, assign an index
+            if query_sequence not in sequence_to_index:
+                index = len(sequence_to_index)
+                sequence_to_index[query_sequence] = index
+                index_to_sequence[index] = query_sequence
+
+            # Check if the subject sequence is in the dictionary; if not, assign an index
+            if subject_sequence not in sequence_to_index:
+                index = len(sequence_to_index)
+                sequence_to_index[subject_sequence] = index
+                index_to_sequence[index] = subject_sequence
+
+            # Store the similarity score in the sparse matrix
+            query_index = sequence_to_index[query_sequence]
+            subject_index = sequence_to_index[subject_sequence]
+            similarity_scores[(query_index, subject_index)] = similarity_score
+            similarity_scores[(subject_index, query_index)] = similarity_score  # Symmetric matrix
+
+# Determine the number of sequences
+num_sequences = len(sequence_to_index)
+print("Sequences: " + str(num_sequences))
+
+# Create a sparse distance matrix
+distance_matrix = lil_array((num_sequences, num_sequences), dtype=float)
+
+# Fill the sparse distance matrix with the similarity scores
+for (i, j), score in similarity_scores.items():
+    distance_matrix[i, j] = score
+
+distance_matrix = distance_matrix.tocsr()
+sort_graph_by_row_values(distance_matrix)
+
+print("Nonzeros in distance matrix: " + str(distance_matrix.count_nonzero()))
+
+# Compute DBSCAN
+# whether eps is 0.1 or 1, the groups are the same (above the threshold of the blast search)
+db = DBSCAN(metric="precomputed", eps = 1, min_samples=3, n_jobs=-1).fit(distance_matrix)
+labels = db.labels_
+n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
+n_noise_ = list(labels).count(-1)
+
+print("Estimated number of clusters: %d" % n_clusters_)
+print("Estimated number of noise points: %d" % n_noise_)
+
+# Print out the groups with suffix .seq_list.fasta
+# for each label in labels, get all db[labels == label] and print its members
+groups = defaultdict(list)
+for i in range(num_sequences):
+    if labels[i] != 1:
+        groups[labels[i]].append(index_to_sequence[i])
+
+for label, group in groups.items():
+    if len(group) > 3 and len(group) < 100: # TODO: Select maximum group size carefully
+        with open(f"group{label}.seq_list", "w") as fout:
+            for g in group:
+                print(g, file=fout)

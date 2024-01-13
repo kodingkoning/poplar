fdf759adaea9da6b95d7a2ae6930ecdcb114f93f
Elizabeth Koning <erkonin@sandia.gov>
2024-01-13 13:04:44 -0800

    change to ref tree script

--- reference_tree.sh
+++ reference_tree.sh
@@ -1,27 +1,28 @@
 #!/bin/bash
 
-# Input a TSV table from https://www.ncbi.nlm.nih.gov/datasets/genome
+# Input a TSV table from https://www.ncbi.nlm.nih.gov/datasets/genome or a NCBI taxon ID
 
 if [ -z "$1" ]; then
     echo Error: input file name is required
     exit 1
 fi
 
-table=$1
-
-if [ ! -f ${table} ]; then 
-    echo Error: Input file ${catalog} does not exist
-    exit 1
-fi
-
-# TODO: write comments explaining the steps
-# Skip the header line and save accessions from all other lines
-ACCESSIONS=$(tail -n +2 ${table} | cut -f1 | tr '\n' ' ') 
 mkdir accessions
 cd accessions
-echo $ACCESSIONS | tr ' ' '\n' > gb_ids.txt
-../scripts/datasets download genome accession $ACCESSIONS --include genome,cds
-unzip ncbi_dataset.zip
+
+if [ ! -f $1 ]; then 
+    taxon=$1
+    ../scripts/datasets download genome taxon $taxon --reference --include genome,cds
+    unzip ncbi_dataset.zip
+    python ../scripts/nav_jsonl.py ncbi_dataset/data/assembly_data_report.jsonl > gb_ids.txt
+else
+    table=$1
+    ACCESSIONS=$(tail -n +2 ${table} | cut -f1 | tr '\n' ' ')
+    ../scripts/datasets download genome accession $ACCESSIONS --include genome,cds
+    echo $ACCESSIONS | tr ' ' '\n' > gb_ids.txt
+    unzip ncbi_dataset.zip
+fi
+
 sed -nr 's/.*"taxId":([0-9]*).*/\1/p' ncbi_dataset/data/assembly_data_report.jsonl > ncbi_ids.txt
 paste ncbi_ids.txt gb_ids.txt | while read ncbi_id gb_id; do
     lineage=$(efetch -db taxonomy -id $ncbi_id -format xml | sed -nr 's-.*<Lineage>(.*)</Lineage>-\1-p')

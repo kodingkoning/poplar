45274e92cbaedc9da5090611a4a89d04c86266d9
Elizabeth Koning <erkonin@sandia.gov>
2024-06-08 14:21:35 -0700

    removed outdated setup options

--- setup.sh
+++ setup.sh
@@ -2,23 +2,6 @@
 
 # Run this in the location you would like to download the dependencies
 
-usage() { echo "USAGE: -q to set queue name and -t to sex max time for a single job"; exit; }
-
-while getopts ":q:t:h:" o; do
-    case "${o}" in 
-        q)
-            QUEUE=${OPTARG}
-            sed -i "s/#SBATCH -p[^#]*/#SBATCH -p ${QUEUE}/" poplar.sbatch scripts/*.script
-            ;;
-        t)
-            TIME=${OPTARG}
-            sed -i "s/#SBATCH --time[^#]*/#SBATCH --time=${TIME}/" poplar.sbatch scripts/*.script
-            sed -i "s/#SBATCH -t[^#]*/#SBATCH -t ${TIME}/" poplar.sbatch scripts/*.script
-            ;;
-        h) usage;;
-    esac
-done
-
 wget https://ftp.ncbi.nlm.nih.gov/blast/executables/blast+/LATEST/ncbi-blast-2.15.0+-x64-linux.tar.gz
 tar -xf ncbi-blast-2.15.0+-x64-linux.tar.gz
 echo "Add to PATH: " $PWD/ncbi-blast-2.15.0+/bin
